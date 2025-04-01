74ee9d5f30cc3a20874f535486f7d28fb31f4d9b
u5491 <fruitmix246@gmail.com>
2025-04-02 01:41:20 +0900

    restore docs

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "基礎数理 9 - 階数と固有値",
+        "excerpt":"基礎数理 9 - 階数, 固有値 Thm（階数の同値な定義） 行列 $A$ の一次独立な列（行）の最大本数として定義される階数 $\\mathrm{rank}\\ A$ は，$A$ の正則行列の最大次数，および $\\dim \\mathrm{Im}\\ A$ に等しい． 上の同値な定義をうまく使うと，次が示せる： Thm（階数の性質） $\\mathrm{rank}\\ AB \\leq \\min \\lbrace \\mathrm{rank}\\ A, \\mathrm{rank}\\ B \\rbrace$ $S, T$ が正則ならば $\\mathrm{rank}\\ A = \\mathrm{rank}\\ SAT$ $\\forall A, B \\in \\mathbb{R}^{m \\times n}, \\mathrm{rank}\\ (A + B) \\leq \\mathrm{rank}\\...","categories": [],
+        "tags": [],
+        "url": "/kiso09/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/03/22/welcome-to-jekyll.html",
+        "teaser": null
+      }]
